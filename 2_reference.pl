ddaaef4756851226b6d4b67d9ba1527ad2069e27
shivanthika12345 <tilani.slt@gmail.com>
2019-09-11 09:55:29 +0530

    referencing

---
+++ 2_reference.pl
@@ -0,0 +1,13 @@
+#! /usr/bin/perl
+use warnings;
+use strict;
+
+my $x= 10;
+my $xr= \$x;
+
+# change $x via $xr
+$$xr=$$xr * 2;
+
+print("$x\n");   #20
+print("$$xr\n"); #20
+print("$xr\n"); # SCALAR(0xld2e6e4)
